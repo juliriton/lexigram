a3c690ab36901600e12719b56e6edcd7a21a5c83
Julian Ritondale <jritondale@mail.austral.edu.ar>
2025-03-29 17:10:40 -0300

    Initial project setup

---
+++ src/main/java/com/lexigram/app/LexigramApplication.java
@@ -0,0 +1,13 @@
+package com.lexigram.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LexigramApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LexigramApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/lexigram/app/LexigramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lexigram.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LexigramApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
