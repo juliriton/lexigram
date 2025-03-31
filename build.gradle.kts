ae88f06de6b6016c112bb9a37bee09374609354e
Julian Ritondale <jritondale@mail.austral.edu.ar>
2025-03-30 21:56:29 -0300

    Setup basic index landing page

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,8 +19,8 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
